a7a4b83aa0ff352414c7d754493b60fbd0b67f2b
Danilo Machado de Sousa <danilomsou@gmail.com>
2020-09-03 16:23:11 -0300

    Add Styles to our Main component

--- src/components/Main/test.tsx
+++ src/components/Main/test.tsx
@@ -12,4 +12,13 @@ describe('<Main />', () => {
 
     expect(container.firstChild).toMatchSnapshot()
   })
+
+  it('should render the colors correctly ', () => {
+    const { container } = render(<Main />)
+
+    expect(container.firstChild).toHaveStyle({
+      'background-color': '#06092b',
+      color: '#fff'
+    })
+  })
 })
